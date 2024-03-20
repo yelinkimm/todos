34ac5a414754d5597f1464e9c140aed6546b5114
Iylin Kim <kiylin94@gmail.com>
2024-03-20 13:55:11 +0900

    Add recoil selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   id: number;
@@ -6,7 +6,22 @@ export interface IToDo {
   category: "TO_DO" | "DOING" |  "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO"
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: []
+});
+
+// atom으로부터 ouput 변형할 수 있음
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({get}) => {
+    const toDos = get(toDoState); // selector의 내부로 atom을 가지고 올 수 있음
+    const category =  get(categoryState);
+    return toDos.filter(toDo => toDo.category === category);
+  }
 });
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,23 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    setToDos((prevToDos) => {
+      const targetIndex = prevToDos.findIndex(toDo => toDo.id === id);
+      const newToDo = {text, id, category: newCategory};
+      
+      return [...prevToDos.slice(0, targetIndex), newToDo, ...prevToDos.slice(targetIndex+1)];
+    })
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
+      {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To do</button>}
+      {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
     </li>
   )
 }
