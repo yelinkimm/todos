993d618081a0583237a5a309a444454bff9fb919
yelinkimm <kiylin94@gmail.com>
2024-03-22 14:39:22 +0900

    Modify global style and set project

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-}
-
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#9c88ff",
-}
+export const darkTheme: DefaultTheme = {}
