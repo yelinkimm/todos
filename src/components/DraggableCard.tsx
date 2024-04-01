7b043710580748a7008ec9f45ddb3a798953fcf1
yelinkimm <kiylin94@gmail.com>
2024-04-01 15:15:13 +0900

    Modify deleting board item

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,6 +1,8 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { currentDraggingState } from "../atoms";
 
 const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
@@ -11,24 +13,39 @@ const Card = styled.div<{ isDragging: boolean }>`
 `;
 
 interface IDraggableCardProps {
-  toDo: string;
+  toDoId: number;
+  toDoText: string;
   index: number;
 }
 
-function DraggableCard({ toDo, index }: IDraggableCardProps) {
+function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
+  const setDragging = useSetRecoilState(currentDraggingState);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  useEffect(() => {
+    setDragging(isDragging);
+  }, [isDragging, setDragging])
+
   return (
-    <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(magic, snapshot) => (
-        <Card
-          isDragging={snapshot.isDragging}
-          ref={magic.innerRef}
-          {...magic.dragHandleProps}
-          {...magic.draggableProps}
-        >
-          {toDo}
-        </Card>
-      )}
+    <>
+    <Draggable key={toDoId} draggableId={toDoId + ""} index={index}>
+      {(magic, snapshot) => {
+        if (isDragging !== snapshot.isDragging) {
+          setIsDragging((prev) => prev !== snapshot.isDragging ? snapshot.isDragging : prev)
+        }
+        return (
+          <Card
+            isDragging={snapshot.isDragging}
+            ref={magic.innerRef}
+            {...magic.dragHandleProps}
+            {...magic.draggableProps}
+          >
+            {toDoText}
+          </Card>
+        )
+      }}
   </Draggable>
+    </>
   )
 }
 
